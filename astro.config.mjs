0684c3f6d5413e2b3164e6622ee66117eb0ad1f9
schultzh06 <schultzh06@gmail.com>
2026-07-29 17:32:39 -0400

    config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,9 +6,9 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://haydenschultz.dev',
   integrations: [react()],
-
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   }
 });
