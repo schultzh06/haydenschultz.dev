5ebd3a79507e489f5cb6a54d2893fa4803b3cafb
schultzh06 <schultzh06@gmail.com>
2026-07-30 15:37:27 -0400

    dark mode, styling

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,19 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://haydenschultz.dev',
-  integrations: [react()],
+  integrations: [react(), icon()],
+  redirects: { '/projects': '/' },
   vite: {
     plugins: [tailwindcss()],
   },
   markdown: {
-    shikiConfig: { theme: 'vitesse-light' },
-  },
+    shikiConfig: {
+      themes: { light: 'vitesse-light', dark: 'vitesse-dark' },
+    },
+  }
 });
