8ceacea25a8b83df243ee2493997200e0592f414
schultzh06 <schultzh06@gmail.com>
2026-08-01 13:29:22 -0400

    contact in navbar, style tweaks

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
 export const SOCIALS = [
   { name: 'GitHub',   href: 'https://github.com/schultzh06',      icon: 'simple-icons:github' },
   { name: 'LinkedIn', href: 'https://linkedin.com/in/schultzh06',          icon: 'simple-icons:linkedin' },
-  { name: 'Email',    href: 'mailto:hrschultz@wpi.edu',                           icon: 'lucide:mail' },
+  { name: 'Contact',    href: '/contact',                           icon: 'lucide:mail' },
 ] as const;
 
 export interface StackItem {
